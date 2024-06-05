3aad57f12c5136c9accceb76f48571c003606c51
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2024-06-05 13:13:45 -0700

    fix indent

--- server.js
+++ server.js
@@ -23,25 +23,28 @@ port = process.argv[2] ? parseInt(process.argv[2], 0) : port;
 
 function fileTypes(name) {
     if (name.endsWith(".mjs")) {
-       return "application/javascript";
+        return "application/javascript";
     }
     if (name.endsWith(".js")) {
-       return "application/javascript";
+        return "application/javascript";
     }
     if (name.endsWith(".css")) {
-       return "text/css";
+        return "text/css";
     }
     if (name.endsWith(".png")) {
-       return "image/png";
+        return "image/png";
     }
     if (name.endsWith(".svg")) {
-       return "image/svg+xml";
+        return "image/svg+xml";
     }
     if (name.endsWith(".html")) {
-       return "text/html";
+        return "text/html";
     }
     if (name.endsWith(".pdf")) {
-       return "application/pdf";
+        return "application/pdf";
+    }
+    if (name.endsWith(".wasm")) {
+        return "application/wasm";
     }
     return "application/octet-stream";
 }
@@ -185,7 +188,7 @@ function propfind(request, response, pathname) {
             response.setHeader('Content-Type', 'text/plain');
             response.end(JSON.stringify(list));
         });
-  });
+    });
 }
 
 function handleRequest(request, response) {
